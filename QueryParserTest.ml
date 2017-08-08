0eeda73d58b7dd92afcbecb714d88f20e95a55a7
Sam Gronblom <sgronblo@gmail.com>
2017-08-09 00:32:17 +0900

    Add first qcheck property
    - generates random queries, turns them into SQL strings, parses them
    back into queries and compares with the original query
    - currently ending up in an infinite loop for some reason

--- QueryParserTest.ml
+++ QueryParserTest.ml
@@ -21,7 +21,7 @@ let parse_test query_string expected_query test_ctxt =
 (*let parse_column_reference text_ctxt =
     let actual = column_reference "employees.name" (Column_reference (Some "employees", "name"))*)
 
-let parse_simple_all_projection_query =
+ let parse_simple_all_projection_query =
     parse_test "select * from helloworld.csv" (Select (All, Table "helloworld.csv", None))
 
 let parse_simple_projection_query_with_prefixed_columns =
@@ -39,6 +39,108 @@ let parse_where_query_boolean_column_reference =
 let parse_where_query_numeric_comparison =
     parse_test "select * from employees.csv where id = 666" (Select (All, Table "employees.csv", Some (Binary ((Eq Eq_equals), Reference (None, "id"), Numeric_literal 666.))))
 
+let some x = Some x
+
+let unary_op_gen = QCheck.Gen.(
+    oneof [
+        return Unary_minus
+    ]
+)
+
+let binary_op_gen = QCheck.Gen.(
+    oneof [
+        return (Eq Eq_equals);
+        return (Eq Eq_not_equals);
+        return (Comp Comp_greater_than);
+        return (Comp Comp_greater_than_equals);
+        return (Comp Comp_less_than);
+        return (Comp Comp_less_than_equals);
+        return (Term Term_minus);
+        return (Term Term_plus);
+        return (Factor Factor_div);
+        return (Factor Factor_mult);
+    ]
+)
+
+let expression_generator column_names = QCheck.Gen.(sized @@ fix
+    (fun self n ->
+        print_endline ("expression " ^ string_of_int n);
+        match n with
+        | 0 -> frequency [
+            (1, return Nil);
+            (2, bool >|= fun b -> Boolean_literal b);
+            (2, small_string >|= fun s -> String_literal s);
+            (2, float >|= fun n -> Numeric_literal n);
+            (2, oneofl column_names >|= fun c -> Reference (None, c))
+        ]
+        | n -> frequency [
+            (1, map2 (fun op e -> Unary (op, e)) unary_op_gen (self (n - 1)));
+            (3, map3 (fun op e e2 -> Binary (op, e, e2)) binary_op_gen (self (n - 1)) (self (n - 1)));
+        ]
+    ))
+
+let char_sequence start_char end_char =
+    let start_ascii = Core.Char.to_int start_char in
+    let end_ascii = Core.Char.to_int end_char in
+    String.init (end_ascii - start_ascii) (fun i -> Core.Char.of_int_exn (start_ascii + i))
+
+let identifier_chars =
+    char_sequence 'a' 'z' ^
+    char_sequence 'A' 'Z'
+
+let identifier_char_gen st = String.get identifier_chars (Random.State.int st (Core.String.length identifier_chars))
+
+let identifier_string_gen =
+    QCheck.string_gen_of_size QCheck.Gen.(1--10) identifier_char_gen
+
+let query_generator = QCheck.Gen.(
+    small_list identifier_string_gen.gen >>= fun column_names ->
+    identifier_string_gen.gen >>= fun table_name ->
+    frequency [
+        (1, return None);
+        (4, map some (expression_generator column_names))
+    ] >>= fun where ->
+    let column_references = List.map (fun c -> Column_reference (None, c)) column_names in
+    let projection = Columns column_references in
+    return (Select (projection, Table table_name, where))
+)
+
+let arbitrary_query = QCheck.make
+    ~print:query_to_string
+    query_generator
+
+let serializing_and_parsing =
+    QCheck.Test.make
+        ~count:1
+        ~name:"parsing a serialized random query"
+        arbitrary_query
+        (fun q ->
+            let query_string = to_query_string q in
+            print_endline ("generated query " ^ query_string);
+            match parse_query query_string with
+            | Ok reconstructed_query ->
+                if q <> reconstructed_query
+                then
+                    let failure_message =
+                        "\n" ^
+                        query_to_string q ^
+                        "\n -> \n" ^
+                        query_string ^
+                        "\n -> \n" ^
+                        query_to_string reconstructed_query in
+                    print_endline failure_message;
+                    QCheck.Test.fail_report failure_message
+                else true
+            | Error msg ->
+                let failure_message =
+                    query_to_string q ^
+                    " -> " ^
+                    query_string ^
+                    " -> " ^
+                    msg in
+                print_endline failure_message;
+                QCheck.Test.fail_report failure_message)
+
 let suite = "select query suite" >:::
     [
         "parsing a simple * query" >:: parse_simple_all_projection_query;
@@ -46,7 +148,8 @@ let suite = "select query suite" >:::
         "parsing a simple query with prefixed columns" >:: parse_simple_projection_query_with_prefixed_columns;
         "parsing a simple query with a where with a boolean literal" >:: parse_where_query_boolean_literal;
         "parsing a simple query with a where with a boolean column reference" >:: parse_where_query_boolean_column_reference;
-        "parsing a simple query with a where with an equals" >:: parse_where_query_numeric_comparison
+        "parsing a simple query with a numeric comparison" >:: parse_where_query_numeric_comparison;
+        QCheck_runner.to_ounit2_test serializing_and_parsing
     ]
 
 let () =
